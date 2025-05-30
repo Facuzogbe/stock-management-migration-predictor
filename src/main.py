ccd526590040be4e6242422bce1d4b46bf794035
Facundo Daniel Zogbe <fzogbe@MacBook-Pro-de-Facundo.local>
2025-05-30 20:32:22 -0300

    Flask application entry point

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
-from app import create_app
+# src/main.py
+from app import create_app  # usa la de __init__.py, que tiene static_folder bien puesto
 
 app = create_app()
 
 if __name__ == "__main__":
+    print(app.url_map)  # debug
     app.run(debug=True)
